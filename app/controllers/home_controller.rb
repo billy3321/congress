7ea66767b2887cd14c47f306b222dd399ef1a324
billy3321 <billy3321@gmail.com>
2014-05-17 17:53:37 +0800

    fix sign_in view

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def legislators
+  end
+
+  def show_legislator
+  end
+
+  def show_video
+  end
+
+  def show_entry
+  end
+end
