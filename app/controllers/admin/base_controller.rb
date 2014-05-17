2055a81d6ea6c226c473368ff543b4603661924d
billy3321 <billy3321@gmail.com>
2014-05-17 16:56:40 +0800

    fix google login

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,15 @@
 class Admin::BaseController < ApplicationController
   layout 'admin'
   before_action :authenticate_user!
+  before_action :require_admin
+
+  private
+
+  def require_admin
+    unless current_user.admin?
+      sign_out current_user
+    end
+  end
+
+
 end
--- db/migrate/20140517054903_devise_create_users.rb
+++ db/migrate/20140517054903_devise_create_users.rb
@@ -2,8 +2,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
+      t.string :name,               null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.boolean :admin,             null: false, default: false
 
       ## Recoverable
       t.string   :reset_password_token
