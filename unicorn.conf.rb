33348ef0e8ed38c8da23da28b93f5ccdfa451bb5
billy3321 <billy3321@gmail.com>
2014-06-12 01:12:16 +0800

    add unicorn config

---
+++ unicorn.conf.rb
@@ -0,0 +1,5 @@
+working_directory "/srv/rails/app"
+listen "/var/run/unicorn.sock", :backlog => 64
+pid "/var/run/unicorn.pid"
+stderr_path "/var/log/unicorn.stderr.log"
+stdout_path "/var/log/unicorn.stdout.log"
