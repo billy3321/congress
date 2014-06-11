85ed8815463f3c2676541dc830418e70b18a40ce
billy3321 <billy3321@gmail.com>
2014-06-11 23:55:57 +0800

    fix index and legislator

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,16 @@
 class HomeController < ApplicationController
   def index
+    videos = Video.order(created_at: :desc).first(5)
+    @main_video = videos.shift
+    @sub_videos = videos
   end
 
   def legislators
+    @legislators = Legislator.order(name: :asc).to_a
   end
 
   def show_legislator
+    @legislator = Legislator.find(params[:id])
   end
 
   def show_video
